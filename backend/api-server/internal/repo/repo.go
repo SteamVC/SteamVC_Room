24600a99e1dc5ea88f0443a79966c8b966ce6eb1
fuyu <88916226+fuyu28@users.noreply.github.com>
2025-11-17 17:40:47 +0900

    feat:移植してみた

---
+++ backend/api-server/internal/repo/repo.go
@@ -0,0 +1,20 @@
+package repo
+
+import (
+	"context"
+
+	"github.com/SteamVC/SteamVC_Room/backend/api-server/internal/models"
+)
+
+type RoomRepo interface {
+	CreateRoom(ctx context.Context, room models.Room, ttlSec int) error
+	GetRoom(ctx context.Context, roomId string) (models.Room, bool, error)
+	DeleteRoom(ctx context.Context, roomId string) error
+
+	AddUser(ctx context.Context, roomId string, user models.User, ttlSec int) error
+	RemoveUser(ctx context.Context, roomId, userId string) error
+	ListUser(ctx context.Context, roomId string) ([]models.User, error)
+
+	TouchRoom(ctx context.Context, roomId string, ttlSec int) error
+	ExistsRoom(ctx context.Context, roomId string) (bool, error)
+}
